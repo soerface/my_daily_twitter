5e2e26bbe35554671e0bbf1937ab2805963b4ecd
Soeren Wegener <wegener92@gmail.com>
2020-08-10 19:08:50 +0200

    WIP: twitter auth

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import os
 import sys
 
+import tweepy
 from redis import Redis
 
 import logging
@@ -15,10 +16,25 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 redis = Redis(host=os.environ.get('REDIS_HOST', 'redis'), port=os.environ.get('REDIS_PORT', 6379))
 
 
+def authorize_twitter():
+    if 'TWITTER_CLIENT_ID' not in os.environ:
+        logging.error('You need to set the environment variable "TWITTER_CLIENT_ID"')
+        return sys.exit(1)
+    if 'TWITTER_CLIENT_SECRET' not in os.environ:
+        logging.error('You need to set the environment variable "TWITTER_CLIENT_SECRET"')
+        return sys.exit(1)
+    auth = tweepy.OAuthHandler(os.environ['TWITTER_CLIENT_ID'], os.environ['TWITTER_CLIENT_SECRET'])
+    print(auth.get_authorization_url())
+
+
 def main():
     if 'TELEGRAM_TOKEN' not in os.environ:
         logging.error('You need to set the environment variable "TELEGRAM_TOKEN"')
-        return sys.exit(-1)
+        return sys.exit(1)
+
+    authorize_twitter()
+
+    sys.exit(10)
     updater = Updater(token=os.environ.get('TELEGRAM_TOKEN'), use_context=True)
     updater.bot.set_my_commands([
         BotCommand('start', 'Returns a warming welcome message'),
